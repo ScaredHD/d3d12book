3c8de24095378fe275f0e1ea821e363f4a1a2701
Li Jianping <1196934490@qq.com>
2024-08-04 21:58:42 +0800

    formatting

---
+++ Common/My/ConstantBuffer.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "Common/d3dUtil.h"
+
+template <typename T>
+class CBuffer {
+  public:
+    CBuffer(ID3D12Device* device, UINT elementCount);
+
+    CBuffer(const CBuffer& other) = delete;
+    CBuffer(CBuffer&& other) noexcept = delete;
+    CBuffer& operator=(const CBuffer& other) = delete;
+    CBuffer& operator=(CBuffer&& other) noexcept = delete;
+
+    ~CBuffer() {
+        if (uploadBuffer_) {
+            uploadBuffer_->Unmap(0, nullptr);
+        }
+
+        mappedData_ = nullptr;
+    }
+
+    ID3D12Resource* Resource() const { return uploadBuffer_.Get(); }
+
+    void CopyData(int elementIndex, const T& data) {
+        memcpy(&mappedData_[elementIndex * elementByteSize_], &data, sizeof(T));
+    }
+
+  private:
+    Microsoft::WRL::ComPtr<ID3D12Resource> uploadBuffer_;
+    BYTE* mappedData_ = nullptr;
+
+    UINT elementByteSize_ = 0;
+};
+
+template <typename T>
+CBuffer<T>::CBuffer(ID3D12Device* device, UINT elementCount) {
+    elementByteSize_ = d3dUtil::CalcConstantBufferByteSize(sizeof(T));
+
+    auto heapProp = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+    auto desc = CD3DX12_RESOURCE_DESC::Buffer(elementByteSize_ * elementCount);
+    ThrowIfFailed(device->CreateCommittedResource(&heapProp,
+                                                  D3D12_HEAP_FLAG_NONE,
+                                                  &desc,
+                                                  D3D12_RESOURCE_STATE_GENERIC_READ,
+                                                  nullptr,
+                                                  IID_PPV_ARGS(&uploadBuffer_)));
+
+    ThrowIfFailed(uploadBuffer_->Map(0, nullptr, reinterpret_cast<void**>(&mappedData_)));
+}
